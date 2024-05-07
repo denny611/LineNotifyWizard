5e8377ca577cd9b7c24bb33b644486db56c2c7e3
daniel <danielcylee@outlook.com>
2024-05-07 13:09:44 +0800

    NotificationListener

---
+++ app/src/main/java/com/daniel/linenotifywizard/NotificationListener.java
@@ -0,0 +1,11 @@
+package com.daniel.linenotifywizard;
+
+import android.app.Service;
+import android.service.notification.NotificationListenerService;
+
+public class NotificationListener extends NotificationListenerService {
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+    }
+}
