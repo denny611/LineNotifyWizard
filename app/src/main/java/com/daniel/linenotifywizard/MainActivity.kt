40d344e89afa632e507f7c2110c31fed4a2a506b
daniel <danielcylee@outlook.com>
2024-05-08 11:21:28 +0800

    request_notify permission

--- app/src/main/java/com/daniel/linenotifywizard/MainActivity.kt
+++ app/src/main/java/com/daniel/linenotifywizard/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.daniel.linenotifywizard
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import com.daniel.linenotifywizard.ui.theme.LineNotifyWizardTheme
 
+
 class MainActivity : ComponentActivity() {
+    lateinit var mActivity:Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mActivity = this
         setContent {
             LineNotifyWizardTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +37,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
     }
 }
 
@@ -35,6 +47,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    FilledButton()
+}
+
+@Composable
+fun FilledButton() {
+    // Create a launcher for starting the activity
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { /* Handle the result if needed */ }
+    Button(
+        onClick = {
+            val intent = Intent(android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+            // Start the activity using the launcher
+            launcher.launch(intent)
+        }
+    ){
+        Text("Request Permission")
+    }
 }
 
 @Preview(showBackground = true)
